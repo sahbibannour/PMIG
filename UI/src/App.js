386089c28eed403794504a0f5dc7909ab90737f6
sahbi bannour <sahbibannour25@gmail.com>
2024-02-29 15:36:36 +0100

    Integrate UI React for Control ETL

---
+++ UI/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          DATA MIGRATION SOLUTION.
+        </p>
+        <br/>
+        <button type="button" className="button">New Project</button>
+        <br/>
+        <button type="button" className="button">Open Project</button>
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
